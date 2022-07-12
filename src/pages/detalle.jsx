6a6d1900721889ece0950c161b8d83c989370b4b
OmiCremer <87448345+OmiCremer@users.noreply.github.com>
2022-07-12 16:08:33 -0300

    D mayuscula
    omi

--- src/pages/detalle.jsx
+++ src/pages/detalle.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import {unaImpresora} from '../service/impresoraService'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { unaImpresora } from "../service/impresoraService";
 export default function Detalle() {
-  const {id} = useParams()
-  const [impresoraID, setImpresorasID] = useState({})
+  const { id } = useParams();
+  const [impresoraID, setImpresorasID] = useState({});
 
   useEffect(() => {
     const oneImpresora = () => {
       try {
-        const response = unaImpresora(id)
-        setImpresorasID(response)
-      }catch(error){
-        console.error(error)
+        const response = unaImpresora(id);
+        setImpresorasID(response);
+      } catch (error) {
+        console.error(error);
       }
-    }
-    oneImpresora()
-  },[id])
+    };
+    oneImpresora();
+  }, [id]);
 
   return (
     <div className="producto">
-          <p>{impresoraID.nombre}</p>
-          <p>{impresoraID.id}</p>
-          <img src = {impresoraID.img} alt={impresoraID.nombre}/>
+      <p>{impresoraID.nombre}</p>
+      <p>{impresoraID.id}</p>
+      <img src={impresoraID.img} alt={impresoraID.nombre} />
     </div>
-  )
+  );
 }
