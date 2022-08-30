bb7ec70731d5aca682f6ccdb10dd35da0dd8812e
OmiCremer <87448345+OmiCremer@users.noreply.github.com>
2022-08-30 11:26:17 -0300

    pagina en construccion

--- printer/src/pages/Home.jsx
+++ printer/src/pages/Home.jsx
@@ -1,37 +1,40 @@
 import React from "react";
-import Printers from '../components/Printers'
-import {Carousel} from 'react-bootstrap';
-import Imagen1 from '../assets/img/carrusel1.webp'
-import Imagen2 from '../assets/img/carrusel2.webp'
+import Printers from "../components/Printers";
+import { Carousel } from "react-bootstrap";
+import Imagen1 from "../assets/img/FD5193ESPrinter.png";
+import Imagen2 from "../assets/img/buildingweb.jpeg";
 
 export default function Home() {
   return (
     <div className="home">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block img"
-          src={Imagen1}
-          alt={Imagen1}
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block img"
-          src={Imagen2}
-          alt={Imagen2}
-        />
+        <Carousel.Item>
+          <img className="d-block img" src={Imagen1} alt={Imagen1} />
+          <Carousel.Caption>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* <Carousel.Item>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block img" src={Imagen2} alt={Imagen2} />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        {/* <Carousel.Item>
         <img
           className="d-block img"
           src="holder.js/800x400?text=Third slide&bg=20232a"
@@ -45,8 +48,8 @@ export default function Home() {
           </p>
         </Carousel.Caption>
       </Carousel.Item> */}
-    </Carousel>
-      <Printers/>
+      </Carousel>
+      <Printers />
     </div>
   );
 }
